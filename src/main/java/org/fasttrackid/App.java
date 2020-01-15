fd6b9e252cde4ce7a23bbf8cd121f608b4181e3d
GergelyIstvanRobert <skiridershop@gmail.com>
2020-01-15 21:32:25 +0200

    First connection

--- src/main/java/org/fasttrackid/App.java
+++ src/main/java/org/fasttrackid/App.java
@@ -1,13 +1,24 @@
 package org.fasttrackid;
 
+import org.persistance.TaskRepository;
+import org.transfer.CreateTaskRequest;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException, SQLException {
+        TaskRepository taskRepository = new TaskRepository();
+        CreateTaskRequest request = new CreateTaskRequest();
+        request.setDescription("learn JDBC");
+        request.setDeadline(LocalDate.now().plusWeeks(1));
+
+        taskRepository.createTask(request);
     }
 }
